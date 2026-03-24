796530333232fdc70f65df5152a8fd99fa4a7089
prishachoithani <prishachoithani@gmail.com>
2026-03-24 18:10:21 +0530

    Create train_models.py

---
+++ train_models.py
@@ -0,0 +1,23 @@
+from sklearn.linear_model import LogisticRegression
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.tree import DecisionTreeClassifier
+
+def train_models(X_train, X_train_scaled, y_train):
+    models = {}
+
+    # Logistic Regression
+    lr = LogisticRegression()
+    lr.fit(X_train_scaled, y_train)
+    models["Logistic Regression"] = lr
+
+    # KNN
+    knn = KNeighborsClassifier(n_neighbors=5)
+    knn.fit(X_train_scaled, y_train)
+    models["KNN"] = knn
+
+    # Decision Tree
+    dt = DecisionTreeClassifier(max_depth=5)
+    dt.fit(X_train, y_train)
+    models["Decision Tree"] = dt
+
+    return models
