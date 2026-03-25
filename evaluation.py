ece260d720473ea84f6e3a67718bab17ab6e8e76
prishachoithani <prishachoithani@gmail.com>
2026-03-25 19:02:40 +0530

    evaluation.py
    Modified evaluate_models function to write results to a file.

--- evaluation.py
+++ evaluation.py
@@ -3,17 +3,22 @@ from sklearn.metrics import accuracy_score, classification_report
 def evaluate_models(models, X_test, X_test_scaled, y_test):
     results = {}
 
-    for name, model in models.items():
-        if name == "Decision Tree":
-            y_pred = model.predict(X_test)
-        else:
-            y_pred = model.predict(X_test_scaled)
+    with open("results.txt", "w") as f:
+        for name, model in models.items():
+            if name == "Decision Tree":
+                y_pred = model.predict(X_test)
+            else:
+                y_pred = model.predict(X_test_scaled)
 
-        acc = accuracy_score(y_test, y_pred)
-        results[name] = acc
+            acc = accuracy_score(y_test, y_pred)
+            results[name] = acc
 
-        print(f"\n{name}")
-        print("Accuracy:", acc)
-        print(classification_report(y_test, y_pred))
+            print(f"\n{name}")
+            print("Accuracy:", acc)
+            print(classification_report(y_test, y_pred))
+
+            f.write(f"\n{name}\n")
+            f.write(f"Accuracy: {acc}\n")
+            f.write(classification_report(y_test, y_pred))
 
     return results
