6b181376f66e7be0b304414e07cb7b1392aaa727
prishachoithani <prishachoithani@gmail.com>
2026-03-29 00:11:16 +0530

    predict.py

--- predict.py
+++ predict.py
@@ -4,6 +4,6 @@ def predict_diabetes(model, scaler, input_data):
     input_data = np.array(input_data).reshape(1, -1)
     input_data = scaler.transform(input_data)
 
-    prediction = model.predict(input_data)
+    prediction = model.predict(input_data) hahahahahhahahahhahahaha
 
     return "Diabetic Risk" if prediction[0] == 1 else "No Risk"
