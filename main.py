b2187a2925d02759e05490678c887251abe170ac
prishachoithani <prishachoithani@gmail.com>
2026-03-29 12:30:46 +0530

    Fix formatting issue in import statements

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ from src.evaluation import evaluate_models
 
 from src.visualization import (
     plot_heatmap,
-    plot_model_comparison, prishaaaaaaaaaaaaaaa
+    plot_model_comparison, 
     knn_tuning,
     decision_tree_overfit,
     feature_importance,
