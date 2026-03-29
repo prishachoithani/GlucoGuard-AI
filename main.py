1893209acf4e4113f6b323ba73554a7cc9ccb011
prishachoithani <prishachoithani@gmail.com>
2026-03-29 12:36:50 +0530

    Fix import statement for evaluate_models
    Corrected import statement for evaluate_models.

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ sys.path.append(os.path.join(os.path.dirname(__file__), "src")) maaaam
 # Imports
 from src.data_preprocessing import load_and_preprocess
 from src.train_models import train_models
-from src.evaluation import evaluate_models 
+from src.evaluation import evaluate_models  maaaaaaammmmm
 
 from src.visualization import (
     plot_heatmap,
